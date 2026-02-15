d3acbc30a45fd5f79077dc66ddbe6013b185bf45
Vlad <11402577@gsuite.duit.edu.ua>
2026-02-15 18:04:55 +0100

    Add task 6: remove duplicates from sorted array

---
+++ task6_remove_duplicates.py
@@ -0,0 +1,24 @@
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    k = 1  # перший елемент завжди унікальний
+
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
+
+
+if __name__ == "__main__":
+    nums = [1, 1, 2]
+    k = removeDuplicates(nums)
+    print("k =", k)
+    print("nums =", nums[:k])
+
+    nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    k = removeDuplicates(nums)
+    print("k =", k)
+    print("nums =", nums[:k])
